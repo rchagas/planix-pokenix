a846b6a77eef86b0faf78a4a3b3fbad81b36308c
raphael <raphaelchagas_@hotmail.com>
2018-08-27 13:25:14 -0300

    Adição de Funcionalidades

--- js/main.js
+++ js/main.js
@@ -1,17 +1,43 @@
 
-let url = 'https://pokeapi.co/api/v2/pokemon/6/';
-let url_img= 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
-axios.get(url).then(function(response){
-    //console.log(response.data.name);
-    let num;
-    if(response.data.id <100){
-        if(response.data.id<10)
-            num = '00'+response.data.id;
-        else num = '0'+response.data.id;
+let url = 'https://pokeapi.co/api/v2/pokemon/';
+let url_img = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
+let box = document.getElementById('sch-box');
+let btn = document.getElementById('sch-btn');
+let tab = document.getElementById('pokeResult');
+
+ function callPokemon() {
+    let table = '';
+    if (box.value != '') {
+        let busca = box.value.split(' ');
+        box.value = '';
+
+        for (let x of busca) {
+            axios.get(url + x).then(function (result) {
+                console.log(result.data.name);
+                let num;
+                if (result.data.id < 100) {
+                    if (result.data.id < 10)
+                        num = '00' + result.data.id;
+                    else num = '0' + result.data.id;
+                }
+                 table += '<tr><td rowspan="3"><img src = '
+                    + url_img + num + '.png ></td><td>Numero:</td><td><h4>'
+                    + num + '</h4></td></tr><tr><td>Nome:</td><td><h4>'
+                    + result.data.name.toUpperCase() + '</h4></td></tr><tr><td>Tipo:</td><td><h4>'
+                    + result.data.types[0].type.name.toUpperCase() + '</h4></td>';
+                /*if (result.data.types[1] !== undefined)
+                        '<td"><h4>' + result.data.types[1].type.name.toUpperCase() + '</h4></td>';*/
+                table+= '</tr>'
+                console.log(table);
+                tab.childNodes[3].innerHTML = table;
+            });
+        }
+        tab.classList.remove('invisible');
     }
-    document.getElementById('pokeResult-img').innerHTML = '<img src = '+url_img+num+'.png >';
-    document.getElementById('pokeResult-num').innerHTML = '<h3>'+num+'</h3>';
-    document.getElementById('pokeResult-name').innerHTML = '<h3>'+response.data.name.toUpperCase()+'</h3>';
-    document.getElementById('pokeResult-type1').innerHTML = '<h3>'+response.data.types[0].type.name.toUpperCase()+'</h3>';
-    document.getElementById('pokeResult-type2').innerHTML = '<h3>'+response.data.types[1].type.name.toUpperCase()+'</h3>';
-});
+}
+
+box.onkeypress = function (e) {
+    if (e.which == 13) btn.click();
+}
+
+btn.addEventListener('click', callPokemon);
